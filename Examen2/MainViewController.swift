bbaa6bf6f2ddbf02456f4660799a5383f8c211f6
JanellyMendoza <janellymendoza18@gmail.com>
2023-12-06 20:34:00 -0600

    Addingpod

--- Examen2/MainViewController.swift
+++ Examen2/MainViewController.swift
@@ -11,19 +11,37 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.isHidden = true
+        view.backgroundColor = .white
+        let manager = NetworkManager()
+        manager.getListSpaceCraft()
+        manager.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func initUI(shipList: [EstrellaDeLaMuerte]){
+        print("Iniciando MainViewController")
+        let shipsView = ShipsView(ships: shipList)
+        view.addSubview(shipsView)
+        shipsView.addAnchors(left: 15, top: 60, right: 15, bottom: 30)
+        shipsView.delegate = self
     }
-    */
 
+}
+
+extension MainViewController: NetworkManagerDelegate, ShipViewDelegate{
+    
+    func responseSucess(response: [EstrellaDeLaMuerte]) {
+        initUI(shipList: response)
+    }
+    
+    func responseError(error: Error) {
+        print("Error::: \(error)")
+    }
+    
+    func shipSelected(ship: EstrellaDeLaMuerte) {
+        print("ship selected")
+        let viewController = DetailShipViewController(ship: ship)
+        viewController.modalPresentationStyle = .fullScreen
+        navigationController?.present(viewController, animated: true)
+    }
 }
